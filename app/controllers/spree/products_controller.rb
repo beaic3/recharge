0d790d94a7a84c585dfea81a33a137de9553e657
Shane Kelly <shanekweb@icloud.com>
2016-04-04 20:14:48 +0100

    bonsai

--- app/controllers/spree/products_controller.rb
+++ app/controllers/spree/products_controller.rb
@@ -16,10 +16,10 @@ module Spree
       @query = params[:q].presence || "*"
       #@products = Spree::Product.search(@query, fields: [:name, :machine_models], highlight: {fields: [:name]})
       if params[:search].present?
-    @products = Spree::Product.__elasticsearch__.search(params[:search]).records
-  else
-    @products = Spree::Product.all
-  end
+        @products = Spree::Product.__elasticsearch__.search(params[:search]).records
+      else
+        @products = Spree::Product.all
+      end
     end
 
 
