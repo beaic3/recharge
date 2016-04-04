f6c0bcd0be695772363962e51839aac36acf7173
Shane Kelly <shanekweb@icloud.com>
2016-04-04 11:48:46 +0100

    elasticsearch rails and model

--- app/models/spree/product.rb
+++ app/models/spree/product.rb
@@ -17,12 +17,12 @@
 # All other variants have option values and may have inventory units.
 # Sum of on_hand each variant's inventory level determine "on_hand" level for the product.
 #
-
+require 'elasticsearch/model'
 module Spree
   class Product < Spree::Base
     searchkick
-    #include Elasticsearch::Model
-    #include Elasticsearch::Model::Callbacks
+    include Elasticsearch::Model
+    include Elasticsearch::Model::Callbacks
 
     
 
