c28a9c83999b8d0f2ad91617bd0458021ef3e483
Shane Kelly <shanekweb@icloud.com>
2016-04-27 15:24:09 +0100

    added carrierwave direct

--- app/controllers/avatar_controller.rb
+++ app/controllers/avatar_controller.rb
@@ -1,7 +1,7 @@
 class AvatarController < ApplicationController
   def new
     @uploader = Spree::User.new.avatar
-    @uploader.success_action_redirect = new_user_url
+    @uploader.success_action_redirect = new_spree_user_url
     @uploader.update_attribute :avatar_key, params[:key]
   end
 end
