9b1dc390a7f7b0ef33f0ee4b6812bede4a1505f7
Shane Kelly <shanekweb@icloud.com>
2016-04-27 15:13:15 +0100

    added carrierwave direct

--- app/controllers/prod_update_controller.rb
+++ app/controllers/prod_update_controller.rb
@@ -1,7 +1,7 @@
 class ProdUpdateController < ApplicationController
 
   def new
-    @uploader = Spree::User.new.avatar
+    @uploader = Spree::User.new.prod_update
     @uploader.success_action_redirect = new_spree_user_url
   end
 
